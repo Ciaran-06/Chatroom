de9d80f81a57ee9055afce0ca3d4490167c85179
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-07-29 01:20:45 +0100

    adding websocket functionalty

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/TutorialEdge/realtime-chat-go-react
 
 go 1.18
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Simple Server")
+	})
+}
 func main() {
   fmt.Println("Chat App v0.01")
+  setupRoutes()
+  http.ListenAndServe(":8080", nil)
 }
