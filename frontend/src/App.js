d401462626334ecdb12a4485b6d580af915d58da
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-07-29 01:41:52 +0100

    added base test for connetivity to websocket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from "reac";
 import './App.css';
+import { connect, sendsMsg } from "../api"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
+
+  send() {
+    console.log("hello");
+    sendsMsg("hello;")
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
